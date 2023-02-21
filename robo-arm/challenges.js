3b27d95251ef06b0130f7cabf042c6d1141f5c54
Amanda Ohannes <126001440+Pigeoncatcher39@users.noreply.github.com>
2023-02-21 18:55:28 +0200

    Create challenges.js

---
+++ robo-arm/challenges.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './scss/CallButton.scss';
+
+//description of the problem
+const CallButton = () => {
+  return (
+      <a href="tel:88007503245" className="call-btn d-none d-xl-flex justify-content-center align-items-center position-fixed">
+]
+    
+export default CallButton;
