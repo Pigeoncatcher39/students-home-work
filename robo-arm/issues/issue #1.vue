731e5affa75c8a97dd07662d4bc68a1c8f7d3a13
Amanda Ohannes <126001440+Pigeoncatcher39@users.noreply.github.com>
2023-02-21 18:52:38 +0200

    Create issue #1.vue

---
+++ robo-arm/issues/issue #1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="brands">
+    <div class="custom-container">
+    
+            </div>
+        <div class="flex-table row" role="rowgroup">
+          <div class="flex-cell" role="cell"><a href="http://alcotec.com.ua/page/electrolux"><img alt="" :src="`${baseDomainImg}/public/uploads/1504273727.jpg`" /></a></div>
+          <div class="flex-cell" role="cell"><a href="http://alcotec.com.ua/page/aeg"><img alt="" :src="`${baseDomainImg}/public/uploads/1504087860.png`" /></a></div>
+          <div class="flex-cell" role="cell"><a href="http://alcotec.com.ua/page/gaggenau"><img alt="" :src="`${baseDomainImg}/public/images/gaggenau(1).jpg`" /></a></div>
+          <div class="flex-cell" role="cell"><a href="http://alcotec.com.ua/page/gorenje"><img alt="" :src="`${baseDomainImg}/public/uploads/1504164331.jpg`" /></a></div>
+        </div>
+        
+        <script>
+import { mapState } from 'vuex';
+import { staticText } from '../i18n';
+import { baseDomainImg } from '../constans';
+export default {
+  name: 'Brands',
+  data() {
+    return {
+    </script>
